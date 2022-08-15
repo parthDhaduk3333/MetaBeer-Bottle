53d0257ee9513be6f9bfa3d21a84d6f43bd845a4
Parth <parthdhaduk207@gmail.com>
2022-08-15 17:00:02 +0530

    api

--- http/index.js
+++ http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://api.metabeerbottle.com",
     headers:{
         'Accept': 'application/json',
         'Content-Type': 'application/json'
